d6a5e9b9a5509e19a56fe2414a5ccb580617faf4
Stefan Aichholzer <theaichholzer@gmail.com>
2018-06-02 17:09:36 +1000

    docs/index.html

--- docs/examples/https.js
+++ docs/examples/https.js
@@ -1,6 +1,6 @@
 /* eslint no-console: 0 */
 
-const https = require('https');
+const https = require('docs/examples/https');
 const { readFileSync } = require('fs');
 const rayo = require('../../bin/rayo');
 
