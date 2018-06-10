e9deeffc10ab432d07b9dcea7cfe80ce0924f21e
Stefan Aichholzer <play@analogbird.com>
2018-06-10 12:16:36 +1000

    - Vary. - Publish @rayo/compress@1.1.0

--- test/units/plugins/compress.js
+++ test/units/plugins/compress.js
@@ -84,7 +84,7 @@ module.exports = () => {
 
   it('transfer-encoding', (done) => {
     const step = press((req, res) => {
-      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('content-type', 'text/plain');
       res.end('Thunderstruck!');
     });
 
@@ -125,7 +125,7 @@ module.exports = () => {
 
   it('res.write() and res.end() -without data', (done) => {
     const step = press((req, res) => {
-      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('content-type', 'text/plain');
       res.write('Hello!');
       res.end();
     });
@@ -140,7 +140,7 @@ module.exports = () => {
 
   it('res.write() and res.end() -with data', (done) => {
     const step = press((req, res) => {
-      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('content-type', 'text/plain');
       res.write('Hello! ');
       res.end('I am compressed!');
     });
@@ -156,7 +156,7 @@ module.exports = () => {
   it('1 Mb body', (done) => {
     const body = Buffer.alloc(1000000, '.');
     const step = press((req, res) => {
-      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('content-type', 'text/plain');
       res.end(body);
     });
 
@@ -172,7 +172,7 @@ module.exports = () => {
   it('10 Mb body', (done) => {
     const body = Buffer.alloc(1e7, '.');
     const step = press((req, res) => {
-      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('content-type', 'text/plain');
       res.end(body);
     });
 
@@ -188,7 +188,25 @@ module.exports = () => {
   it('pipe, 1 Mb body', (done) => {
     const body = Buffer.alloc(1000000, '.');
     const step = press((req, res) => {
-      res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('content-type', 'text/plain');
+      const bufferStream = new PassThrough();
+      bufferStream.pipe(res);
+      bufferStream.end(body);
+    });
+
+    request(step)
+      .get('/')
+      .set('accept-encoding', 'gzip')
+      .expect(header('content-encoding', 'gzip'))
+      .expect(header('transfer-encoding', 'chunked'))
+      .expect(matchSize(body.length))
+      .expect(200, done);
+  });
+
+  it('pipe, 10 Mb body', (done) => {
+    const body = Buffer.alloc(1e7, '.');
+    const step = press((req, res) => {
+      res.setHeader('content-type', 'text/plain');
       const bufferStream = new PassThrough();
       bufferStream.pipe(res);
       bufferStream.end(body);
@@ -211,7 +229,7 @@ module.exports = () => {
       power: 'reduction'
     });
     const step = press((req, res) => {
-      res.setHeader('Content-Type', 'application/json');
+      res.setHeader('content-type', 'application/json');
       res.end(json);
     });
 
@@ -221,6 +239,97 @@ module.exports = () => {
       .expect(header('content-type', 'application/json'))
       .expect(header('content-encoding', 'gzip'))
       .expect(header('transfer-encoding', 'chunked'))
+      .expect(200, json, done);
+  });
+
+  it('vary header', (done) => {
+    const json = JSON.stringify({
+      message: 'Thunderstruck',
+      age: 20,
+      name: 'Rayo',
+      power: 'reduction'
+    });
+    const step = press((req, res) => {
+      res.setHeader('content-type', 'application/json');
+      res.end(json);
+    });
+
+    request(step)
+      .get('/')
+      .set('accept-encoding', 'gzip')
+      .expect(header('content-type', 'application/json'))
+      .expect(header('content-encoding', 'gzip'))
+      .expect(header('transfer-encoding', 'chunked'))
+      .expect(header('vary', 'content-encoding'))
+      .expect(200, json, done);
+  });
+
+  it('vary header, set single', (done) => {
+    const json = JSON.stringify({
+      message: 'Thunderstruck',
+      age: 20,
+      name: 'Rayo',
+      power: 'reduction'
+    });
+    const step = press((req, res) => {
+      res.setHeader('vary', 'content-type');
+      res.setHeader('content-type', 'application/json');
+      res.end(json);
+    });
+
+    request(step)
+      .get('/')
+      .set('accept-encoding', 'gzip')
+      .expect(header('content-type', 'application/json'))
+      .expect(header('content-encoding', 'gzip'))
+      .expect(header('transfer-encoding', 'chunked'))
+      .expect(header('vary', 'content-type, content-encoding'))
+      .expect(200, json, done);
+  });
+
+  it('vary header, set multiple', (done) => {
+    const json = JSON.stringify({
+      message: 'Thunderstruck',
+      age: 20,
+      name: 'Rayo',
+      power: 'reduction'
+    });
+    const step = press((req, res) => {
+      res.setHeader('vary', 'content-type, transfer-encoding');
+      res.setHeader('content-type', 'application/json');
+      res.end(json);
+    });
+
+    request(step)
+      .get('/')
+      .set('accept-encoding', 'gzip')
+      .expect(header('content-type', 'application/json'))
+      .expect(header('content-encoding', 'gzip'))
+      .expect(header('transfer-encoding', 'chunked'))
+      .expect(header('vary', 'content-type, transfer-encoding, content-encoding'))
+      .expect(200, json, done);
+  });
+
+  it('vary header, set multiple (content-encoding set)', (done) => {
+    const json = JSON.stringify({
+      message: 'Thunderstruck',
+      age: 20,
+      name: 'Rayo',
+      power: 'reduction'
+    });
+    const step = press((req, res) => {
+      res.setHeader('vary', 'content-type, transfer-encoding, content-encoding');
+      res.setHeader('content-type', 'application/json');
+      res.end(json);
+    });
+
+    request(step)
+      .get('/')
+      .set('accept-encoding', 'gzip')
+      .expect(header('content-type', 'application/json'))
+      .expect(header('content-encoding', 'gzip'))
+      .expect(header('transfer-encoding', 'chunked'))
+      .expect(header('vary', 'content-type, transfer-encoding, content-encoding'))
       .expect(200, json, done);
   });
 };
