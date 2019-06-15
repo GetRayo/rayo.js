b7f15719d9139bed681fe367d77af641d669b3e0
Stefan Aichholzer <theaichholzer@gmail.com>
2019-06-15 23:25:31 +1000

    `notFound` method to have access to `middleware.`

--- packages/rayo/index.js
+++ packages/rayo/index.js
@@ -56,29 +56,24 @@ class Rayo extends Bridge {
   }
 
   dispatch(req, res) {
-    const through = this.t;
-
     const parsedUrl = parseurl(req);
     req.ip = ip(req);
     req.pathname = parsedUrl.pathname;
     req.query = parse(parsedUrl.query);
 
+    let stack;
     const route = this.fetch(req.method, parsedUrl.pathname);
     if (!route) {
-      return this.step(
-        req,
-        res,
-        through.concat([
-          this.notFound
-            ? this.notFound
-            : () =>
-                end(req, res, 404, `${req.method} ${parsedUrl.pathname} is undefined.`)
-        ])
-      );
+      stack = [
+        this.notFound ||
+          (() => end(req, res, 404, `${req.method} ${parsedUrl.pathname} is undefined.`))
+      ];
+    } else {
+      req.params = route.params;
+      ({ stack } = route);
     }
 
-    req.params = route.params;
-    return this.step(req, res, through.concat(route.stack));
+    return this.step(req, res, this.t.concat(stack));
   }
 
   step(req, res, stack, error = null, statusCode = 400) {
